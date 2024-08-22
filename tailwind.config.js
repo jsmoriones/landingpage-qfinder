b56169b87da8da11f37c49eab5abc99014462d11
jsmoriones <jsmoriones@misena.edu.co>
2024-08-22 08:38:43 -0500

    Agregando menú superior e imagen de portada con degradado

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "azul": "#4B9AF6",
+        "verdeOscuro": "#048C22",
+        "verdeClaro": "#13BA6A",
+        "azulMyClaro": "#84B6F4"
+      },
+      backgroundImage: {
+        'slide1': "linear-gradient(90deg, #84B6F4 -10%, transparent 90%), url('images/slide-1.jpg')",
+      }
+    },
   },
   plugins: [],
 }
