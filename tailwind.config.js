d516a69accdb19f357d43b0717d07845b3f14a2a
jsmoriones <jsmoriones@misena.edu.co>
2024-08-23 11:16:00 -0500

    Terminando apartado de nuestros servicios

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,13 @@ export default {
         "azulMyClaro": "#84B6F4",
         "purpuraAzul": "#271E4A",
         "azulAgua": "#70C8FA4A",
-        "rojo": "#CA0000"
+        "rojo": "#CA0000",
+        "grisTrasparente": "rgba(242,242,242, 53%)",
+        "grisTexto": "#585757"
       },
       backgroundImage: {
         'slide1': "linear-gradient(90deg, #84B6F4 -10%, transparent 90%), url('images/slide-1.jpg')",
+        'bgServicios': "linear-gradient(rgba(0, 117, 255, 17%), rgba(0, 117, 255, 17%)), url('images/bg-servicios.png')"
       }
     },
   },
