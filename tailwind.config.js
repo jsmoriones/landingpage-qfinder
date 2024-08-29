696b694b03132e79d81ac3398b52a1fcebe0b17c
jsmoriones <jsmoriones@misena.edu.co>
2024-08-29 10:47:56 -0500

    Se agrego apartado donde se muestra un ceular y unos botones para descargar la app

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,13 +12,16 @@ export default {
         "azulAgua": "#70C8FA4A",
         "rojo": "#CA0000",
         "grisTrasparente": "rgba(242,242,242, 53%)",
-        "grisTexto": "#585757"
+        "grisTexto": "#585757",
+        "azulClApp1": "#c3deff",
+        "textoGris": "#858585"
       },
       backgroundImage: {
         'slide1': "linear-gradient(90deg, #84B6F4 -10%, transparent 90%), url('images/slide-1.jpg')",
         'bgServicios': "linear-gradient(rgba(0, 117, 255, 17%), rgba(0, 117, 255, 17%)), url('images/bg-servicios.png')",
         'dapp': 'linear-gradient(to right, #FFF -2%, #FFF 50%, #B6D2FC 50%)',
-        'bgAbuelaMedico': 'url(/images/elipse-abuela-medico.png)'
+        'bgAbuelaMedico': 'url(/images/elipse-abuela-medico.png)',
+        'cldescarga': 'url(/images/elipse-movil.png)'
       },
       screens: {
         'mq1360': { 'raw': '(min-width: 1360px)' },
@@ -30,7 +33,17 @@ export default {
         "sliderShadowText": "2px 2px 1px #00000030"
       },
       backgroundSize: {
-        "92%": "92%"
+        "92%": "92%",
+        "65%": "65%"
+      },
+      keyframes: {
+        celular: {
+          '0%': { transform: 'translateY(0deg)' },
+          '50%': { transform: 'translateY(-20px)' },
+        }
+      },
+      animation: {
+        celular: 'celular 2s linear infinite',
       }
     },
   },
